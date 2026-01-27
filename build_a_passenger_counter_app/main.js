7219deb8cc7e790043006a377fde6eb07ffed4d4
Pamir Açar <pamiracar.dev@gmail.com>
2026-01-27 22:49:17 +0300

    Add .idea config files and update main.js logic
    Added JetBrains IDE project files for module and VCS configuration. Updated main.js to include a second batch variable, calculate total count, and compute dog age based on a human-to-dog ratio.

--- build_a_passenger_counter_app/main.js
+++ build_a_passenger_counter_app/main.js
@@ -1,10 +1,17 @@
 // document.getElementById("counter-el").innerText = 5;
 
-//let myAge = 16;
+let myAge = 16;
 //console.log(myAge);
 
 let firstBatch = 5;
+let secondBatch = 7;
 
-let count = firstBatch;
+let count = firstBatch + secondBatch;
 console.log(count);
+
+console.log("----------");
+
+let humanDogRatio = 7;
+let myDogAge = myAge*humanDogRatio;
+console.log(myDogAge);
 
