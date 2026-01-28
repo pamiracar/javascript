dd6e8d6b605da670e30fbbe8ba84a1ff00051262
Pamir Açar <pamiracar.dev@gmail.com>
2026-01-28 19:33:58 +0300

    Clean up main.js and simplify increment/decrement
    Removed commented-out code and refactored increment and decrement functions to use shorthand operators for clarity and conciseness.

--- build_a_passenger_counter_app/main.js
+++ build_a_passenger_counter_app/main.js
@@ -1,41 +1,16 @@
-// document.getElementById("counter-el").innerText = 5;
-//
-// let myAge = 16;
-// //console.log(myAge);
-//
-// let firstBatch = 5;
-// let secondBatch = 7;
-//
-// let count = firstBatch + secondBatch  
-// console.log(count);
-//
-// console.log("----------");
-//
-// let humanDogRatio = 7;
-// let myDogAge = myAge*humanDogRatio;
-// console.log(myDogAge);
-//
-// console.log("----------");
-//
-// let bonusPoints = 50;
-// bonusPoints = bonusPoints + 50;
-// bonusPoints = bonusPoints - 75;
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints)
-
 let count = 0;
 let countEl = document.getElementById("counter-el");
 let prev_entry = document.getElementById("prev-entry");
 
 function increment() {
     console.log("Increment button was clicked!");
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
 }
 
 function decrement() {
     console.log("Decrement button was clicked!");
-    count = count - 1;
+    count -= 1;
     countEl.innerText = count;
 }
 
