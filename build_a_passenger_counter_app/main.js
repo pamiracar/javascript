b59e1c8b44f748ca097abfeeecb1a77b1d3d5f48
Pamir Açar <pamiracar.dev@gmail.com>
2026-01-28 19:23:51 +0300

    Add increment, decrement, and save functionality
    Introduced increment, decrement, and save buttons to the passenger counter app. Updated JavaScript to handle button actions and display previous entries. Adjusted CSS for new button styles and layout.

--- build_a_passenger_counter_app/main.js
+++ build_a_passenger_counter_app/main.js
@@ -1,17 +1,46 @@
 // document.getElementById("counter-el").innerText = 5;
+//
+// let myAge = 16;
+// //console.log(myAge);
+//
+// let firstBatch = 5;
+// let secondBatch = 7;
+//
+// let count = firstBatch + secondBatch  
+// console.log(count);
+//
+// console.log("----------");
+//
+// let humanDogRatio = 7;
+// let myDogAge = myAge*humanDogRatio;
+// console.log(myDogAge);
+//
+// console.log("----------");
+//
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50;
+// bonusPoints = bonusPoints - 75;
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints)
 
-let myAge = 16;
-//console.log(myAge);
+let count = 0;
+let countEl = document.getElementById("counter-el");
+let prev_entry = document.getElementById("prev-entry");
 
-let firstBatch = 5;
-let secondBatch = 7;
+function increment() {
+    console.log("Increment button was clicked!");
+    count = count + 1;
+    countEl.innerText = count;
+}
 
-let count = firstBatch + secondBatch;
-console.log(count);
+function decrement() {
+    console.log("Decrement button was clicked!");
+    count = count - 1;
+    countEl.innerText = count;
+}
 
-console.log("----------");
-
-let humanDogRatio = 7;
-let myDogAge = myAge*humanDogRatio;
-console.log(myDogAge);
+function save() {
+    console.log(count);
+    prev_entry.innerText = prev_entry.innerText + count + " - ";
+}
 
